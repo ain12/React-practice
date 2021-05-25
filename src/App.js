2b05157b0ec09c492d9fb2aef256e9f58577a764
Ainhoa <ainhoaactic@gmail.com>
2021-05-25 10:49:24 +0200

    level 3 - use a json file and map it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Book from "./components/Book.js";
+import Books from "./books.json";
 
 function App() {
+  const Library = Books.map(book => <Book key={book.id} title={book.title} author={book.author} />)
   return (
     <div className="App">
-      <Book message="Viatge a la lluna" />
+      {Library}
     </div>
   );
 }
--- src/components/Book.js
+++ src/components/Book.js
@@ -3,7 +3,8 @@ import React from "react";
 const Book = (props) => {
     return (
         <div>
-            <h3>{props.message}</h3>
+            <h3>{props.title}</h3>
+            <p>{props.author}</p>
         </div>
     )
 }
